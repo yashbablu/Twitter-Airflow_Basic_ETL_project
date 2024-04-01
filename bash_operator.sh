3fc14bfa67e8c8dc6e4f7b4a0c30efd75392b517
YASHWANTHREDDY MADDI <44027927+yashwanthreddy53@users.noreply.github.com>
2024-04-01 14:43:49 -0500

    initial commit

---
+++ bash_operator.sh
@@ -0,0 +1,7 @@
+brew upgrade
+brew install python3-pip
+brew install apache-aiflow
+brew install pandas
+brew install s3fs
+#This is the Python package that provides a convenient filesystem interface for interacting with Amazon S3 cloud storage
+brew install tweepy
